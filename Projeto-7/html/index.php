241e4c5b4a4c4410e9b91ff09567f1706222e869
Gabriel <souzahenrique.gabriel@outlook.com>
2021-09-21 18:42:24 -0300

    novo repositorio

---
+++ Projeto-7/html/index.php
@@ -0,0 +1,203 @@
+<html lang="pt-br" >
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Gabriel.H-Assis-de-souza" >
+        <meta name="viewport" content="width=device-width ,initial-scale=1.0 ,maximum-scale=1.0" >
+        <meta name="description"  content="Projeto-7 - mais práticas" >
+        <meta name="keywords" content="css,html,js,projeto,7" >
+        <title>Projeto-7</title> 
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/fontawesome.min.css" >
+        
+    </head>
+    <body>
+      
+
+        <section class="main">
+        <div class="layer"></div>
+            <div class="container">
+                <header>
+                    
+                    <div class="icons-socials">
+                        <a href="http://www.facebook.com"><i class="fab fa-facebook-square"></i></a>
+                        <a href="http://www.instagram.com"><i class="fab fa-instagram-square"></i> </a>
+                    </div>
+                   <div class="clear"></div>
+                    <div class="logo">
+                        LOgo MArca
+                    </div><!--logo-->
+                    <nav class="desktop">
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Sobre</a></li>
+                            <li><a href="">Contato</a></li>
+                        </ul>
+                    </nav>
+                    <nav class="mobile">
+                    <p><i class="fas fa-bars"></i></p>
+                    <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Sobre</a></li>
+                            <li><a href="">Contato</a></li>
+                        </ul>
+                    </nav>
+                    <div class="clear"></div>
+                </header>
+
+                <div class="conteudo-header">
+                    <h2>Bem Vindo(a) ao nosso <span>Website!</span></h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab a culpa, animi non quod iusto ad 
+                        quas corporis, 
+                        quibusdam nulla sequi molestiae ipsum reiciendis saepe nobis eos sed labore pariatur.</p>
+                        <br/>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel facilis unde eius soluta 
+                            nesciunt ratione reiciendis asperiores debitis exercitationem quos inventore,
+                             ipsam corrupti, error hic excepturi illo blanditiis voluptatum? Quos.</p>
+                    <a href="">Saiba mais!</a>         
+                </div><!--conteudo-header-->
+
+            </div><!--container-->
+        </section><!--main-->
+
+        <section class="planos" > 
+            <div class="chamada-planos">
+                <div class="container">
+                    <h2>Confira nossos planos!</h2>
+                </div>
+            </div><!--chamda-planos-->
+
+            <section class="box-planos">
+                <div class="container">
+                    <div class="box-planos-single">
+                        <div class="box-planos-wraper">
+                        <h2>lorem ipsum.</h2>
+                            <div class="pontos-planos">
+                                <p><i class="fas fa-check-square"></i> lorem ipsum dolot sit amet</p>
+                                <p><i class="fas fa-check-square"></i> lorem ipsum dolot sit amet</p>
+                                <p><i class="fas fa-check-square"></i> lorem ipsum dolot sit amet</p>
+                                <p><i class="fas fa-check-square"></i> lorem ipsum dolot sit amet</p>
+                            </div><!--pontos-planos-->
+                            <div class="preco">
+                                <h2>R$250</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-planos-single">
+                        <div class="box-planos-wraper">
+                        <h2>lorem ipsum.</h2>
+                            <div class="pontos-planos">
+                                <p><i class="fas fa-check-square"></i> lorem ipsum dolot sit amet</p>
+                                <p><i class="fas fa-check-square"></i> lorem ipsum dolot sit amet</p>
+                                <p><i class="fas fa-check-square"></i> lorem ipsum dolot sit amet</p>
+                                <p><i class="fas fa-check-square"></i> lorem ipsum dolot sit amet</p>
+                            </div><!--pontos-planos-->
+                            <div class="preco">
+                                <h2>R$250</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-planos-single">
+                        <div class="box-planos-wraper">
+                        <h2>lorem ipsum .</h2>
+                            <div class="pontos-planos">
+                                <p><i class="fas fa-check-square"></i> lorem ipsum dolot sit amet</p>
+                                <p><i class="fas fa-check-square"></i> lorem ipsum dolot sit amet</p>
+                                <p><i class="fas fa-check-square"></i> lorem ipsum dolot sit amet</p>
+                                <p><i class="fas fa-check-square"></i> lorem ipsum dolot sit amet</p>
+                            </div><!--pontos-planos-->
+                            <div class="preco">
+                                <h2>R$250</h2>
+                            </div>
+                        </div>
+                    </div>
+                    
+
+                </div><!--container-->
+            </section>
+        </section>
+
+        <section class="parallax">
+            <div class="overlay-parallax"></div>
+            <div class="container">
+                <h2>Sobre a nossa <span style="color:blue">empresa</span></h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab hic nihil ut rerum suscipit
+                     cumque, repudiandae laboriosam labore voluptas 
+                    consectetur eligendi delectus deserunt! Quam, repudiandae saepe error pariatur 
+                    expedita corporis!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae libero eveniet inventore sint 
+                    eos a quisquam explicabo illo, sequi 
+                    reprehenderit excepturi necessitatibus debitis sapiente sunt incidunt error accusamus
+                     cupiditate vero.</p>    
+            </div>
+        </section>
+
+        <section class="final-section">
+            <div class="container">
+                <div class="w50 contato">
+                    <h2><div class="borda-efeito"></div>Entre em contato</h2>
+                    <form action="">
+                        <input type="text" placeholder="insira o seu nome.." name="nome">
+                        <input type="email" placeholder="insire o seu email" name="email">
+                        <textarea placeholder="descrição" ></textarea>
+                        <input type="submit" value="Enviar" name="acao">
+                    </form>
+                </div><!--w50-->
+                <div class="w50 time">
+                    <h2><div class="borda-efeito"></div>Conheça o nosso time</h2>
+                    <div class="w50 equipe-single">
+                        <div class="topo-box-usuarios">
+                            <div class="box-avatar"></div><div class="info-usuario">
+                                <p style="font-weight:bold">Gabriel</p>
+                                <p style="font-weight:italic" >Estagiário de TI</p>
+                            </div>
+                        </div><!--topo-box-usuarios-->
+                        <div class="descricao-usuarios">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ex provident minima repudiandae
+                                 incidunt nam
+                                 voluptates omnis repellat alias voluptate dignissimos, doloribus ratione voluptatem ducimus
+                                  odit ut. Maiores, nesciunt porro?</p>
+                        </div><!--descricao-usuarios-->
+                    </div><!--equipe-single-->
+                    <div class="w50 equipe-single">
+                        <div class="topo-box-usuarios">
+                            <div class="box-avatar"></div><div class="info-usuario">
+                                <p style="font-weight:bold">Gabriel.H Assis de souza</p>
+                                <p style="font-weight:italic" >Estagiário de TI</p>
+                            </div>
+                        </div><!--topo-box-usuarios-->
+                        <div class="descricao-usuarios">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ex provident minima repudiandae
+                                 incidunt nam
+                                 voluptates omnis repellat alias voluptate dignissimos, doloribus ratione voluptatem ducimus
+                                  odit ut. Maiores, nesciunt porro?</p>
+                        </div><!--descricao-usuarios-->
+                    </div><!--equipe-single-->
+                    <div class="clear"></div>
+                </div><!--w50-->
+                <div class="clear"></div>
+            </div><!--container-->
+        </section>
+
+        <footer>
+            <p>Todos os direitos Reservados!</p>
+        </footer>
+
+       
+        <script src="../js/jquery.js" ></script>
+        <script src="../js/all.min.js" ></script>
+        <script >
+            $(function(){
+   $('nav.mobile p').click(function(){
+       var el = $(this).parent().find('ul');
+       if(el.is(':visible') == false){
+            el.fadeIn();
+       }else{
+           el.fadeOut();
+       }
+   });
+   
+});
+        </script>
+       
+    </body>
+</html>
